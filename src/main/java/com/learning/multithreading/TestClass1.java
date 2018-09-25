409cc6e8493de8810d1e574367a42795be1c95ad
Mendiratta <RXM05IL@fpl.com>
2018-09-25 03:39:42 -0400

    Added 2nd session notes

---
+++ src/main/java/com/learning/multithreading/TestClass1.java
@@ -0,0 +1,32 @@
+package com.learning.multithreading;
+
+
+
+class MyThread extends Thread{
+	
+	public void run(){
+		
+		
+		for(int i =0;i<10;i++){
+			System.out.println("Child Thread");
+		}
+	}
+	
+}
+
+public class TestClass1 {
+	
+	
+	public static void main(String[] args){
+		
+		
+		MyThread t1 = new MyThread();
+		t1.start();
+		
+		for(int i =0;i<10;i++){
+			System.out.println("Main Thread");
+		}
+		
+	}
+
+}
